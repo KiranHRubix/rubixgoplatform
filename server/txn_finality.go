fc80878faa3bc7ae56b3501aee658352d5f54f30
Kiran H <kiran.h@rubix.network>
2023-12-08 11:06:18 +0530

    added swagger for txn finality

--- server/txn_finality.go
+++ server/txn_finality.go
@@ -7,8 +7,21 @@ import (
 	"github.com/rubixchain/rubixgoplatform/wrapper/ensweb"
 )
 
+// ShowAccount godoc
+// @Summary      get finality pending txn
+// @Description  For a mentioned DID, check the finality pending txns
+// @Tags         Account
+// @Accept       json
+// @Produce      json
+// @Param        did      	   query      string  true  "User DID"
+// @Success 200 {object} model.PendingTxnIds
+// @Router /api/get-pending-txn [get]
 func (s *Server) APIGetPendingTxn(req *ensweb.Request) (res *ensweb.Result) {
-	response, err := s.c.GetFinalityPendingTxns()
+	did := s.GetQuerry(req, "did")
+	if !s.validateDIDAccess(req, did) {
+		return s.BasicResponse(req, false, "DID does not have an access", nil)
+	}
+	response, err := s.c.GetFinalityPendingTxns(did)
 	if err != nil {
 		s.log.Error("Error", err)
 		result := model.PendingTxnIds{
@@ -35,6 +48,15 @@ func (s *Server) APIGetPendingTxn(req *ensweb.Request) (res *ensweb.Result) {
 	return s.RenderJSON(req, &result, http.StatusOK)
 }
 
+// @Summary Txn Finality by Transcation ID
+// @Description Initiates the process to achieve finality of pending txn.
+// @ID initiate-txn-finality
+// @Tags         Account
+// @Accept json
+// @Produce json
+// @Param txnID query string true "The ID of the transaction pending finality"
+// @Success 200 {object} model.BasicResponse
+// @Router /api/initiate-txn-finality [post]
 func (s *Server) APIInitiateTxnFinality(req *ensweb.Request) (res *ensweb.Result) {
 	txnID := s.GetQuerry(req, "txnID")
 	response := s.c.InitiateRBTTxnFinality(txnID)
